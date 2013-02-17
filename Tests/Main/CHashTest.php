d0f2faecf3e5b1a797e2ac776013149a6d9f5da4
Eduardo Oliveira <eduardo.oliveira@adclick.pt>
2013-02-17 16:33:21 +0000

    Add lookup and tests

--- Tests/Main/CHashTest.php
+++ Tests/Main/CHashTest.php
@@ -87,4 +87,98 @@ class CHashTest extends \PHPUnit_Framework_TestCase
             10.5
         );
     }
+
+    /**************************************************************************
+     * Test lookup.
+     *************************************************************************/
+
+    /**
+     * Test lookup with key as array.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid key.
+     */
+    public function testLookupKeyAsArray()
+    {
+        $this->chash->lookup(array('test'), 2);
+    }
+
+    /**
+     * Test lookup with empty key.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid key.
+     */
+    public function testLookupKeyEmptyString()
+    {
+        $this->chash->lookup('', 2);
+    }
+
+    /**
+     * Test lookup with targets count as array.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Targets count needs to be a positive integer
+     */
+    public function testLookupTargetsCountAsArray()
+    {
+        $this->chash->lookup('test', array(2));
+    }
+
+    /**
+     * Test lookup more targets than exists.
+     */
+    public function testLookupMoreTargetsThanExists()
+    {
+        $targets = array(
+            'test1' => 1,
+            'test2' => 1,
+            'test3' => 1,
+        );
+
+        $this->chash->targets()->addMulti($targets);
+
+        $resTargets = $this->chash->lookup('test', 5);
+
+        $targetsNames = array_keys($targets);
+        sort($targetsNames);
+        sort($resTargets);
+
+        $this->assertCount(count($targetsNames), $resTargets);
+        $this->assertEquals($targetsNames, $resTargets);
+    }
+
+//    public function testLookup()
+//    {
+//        $this->chash->targets()->addMulti(
+//            array(
+//                'test1' => 1,
+//                'test2' => 1,
+//                'test3' => 10,
+//                'test4  ' => 1,
+//                'test5' => 1,
+//                'test6' => 1,
+//                'test7' => 1,
+//            )
+//        );
+//
+//        $targetsDist = array();
+//
+//        for ($i = 1; $i <= 1000; ++$i) {
+//            $targets = $this->chash->lookup($this->generateRandomString(), 1);
+//
+//            foreach ($targets as $target) {
+//                (!isset($targetsDist[$target])) ?
+//                    $targetsDist[$target] = 1 :
+//                    ++$targetsDist[$target];
+//            }
+//        }
+//
+//        print_r($targetsDist);
+//    }
+
+    protected function generateRandomString($length = 20, $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    {
+        return substr(str_shuffle($chars), 0, $length);
+    }
 }
