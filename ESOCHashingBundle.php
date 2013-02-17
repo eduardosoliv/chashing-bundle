c26ad008f9608b2fe86984b50a0015a2a8bbea26
Eduardo Oliveira <eduardo.oliveira@adclick.pt>
2013-02-17 01:28:35 +0000

    Initial commit

---
+++ ESOCHashingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ESO\CHashingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ESOCHashingBundle extends Bundle
+{
+}
---
+++ Tests/DefaultControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ESO\CHashingBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIndex()
+    {
+	$this->assertEquals(1, 1);
+    }
+}
